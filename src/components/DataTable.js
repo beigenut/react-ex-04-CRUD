ad1f51a2d06e618f4b5f0c913911a8ee704bdaa5
Beigenut <beigenut@gmail.com>
2019-09-14 00:24:33 -0400

    useState

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Button, Grommet, Box } from 'grommet'
+import React, { useState } from 'react'
+import { Button, Grommet, Box, Collapsible } from 'grommet'
 import { Notification } from 'grommet-icons'
 
 const customTheme = {
@@ -15,11 +15,43 @@ const customTheme = {
   }
 };
 
+const defaultData = [
+  {
+    "id": "1",
+    "name": "name1",
+    "occupation": "student"
+  }, {
+    "id": "2",
+    "name": "name2",
+    "occupation": "engineer"
+  }, {
+    "id": "3",
+    "name": "name3",
+    "occupation": "doctor"
+  },
+]
 
 const DataTable = () => {
+  const [isAuthorized, setAuthorize] = useState(false)
+  const [data, setData] = useState(defaultData)
+
   return (
     <Grommet theme={customTheme}>
-      <Button style={{ width: '200px' }} primary icon={<Notification />} hoverIndicator onClick={() => { }} />
+      <Button style={{ width: '200px' }} primary icon={<Notification />} label="Load Data" hoverIndicator onClick={() => { setAuthorize(!isAuthorized) }} />
+      <Collapsible direction="vertical" open={isAuthorized}>
+        <Box flex
+          width='medium'
+          background='light-2'
+          elevation='small'
+          align='center'
+          justify='center'>
+          <ul>
+            {data.map(i => (
+              <li>{i.name}</li>
+            ))}
+          </ul>
+        </Box>
+      </Collapsible>
     </Grommet>
   )
 }
