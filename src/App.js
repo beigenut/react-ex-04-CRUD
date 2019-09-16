aae49fc9b8de245f9b97f8eb6123bc0055696b4e
Beigenut <beigenut@gmail.com>
2019-09-16 17:37:19 -0400

    Handle inputs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import { Grommet } from 'grommet'
 import './index.scss';
 import DataTable from './components/DataTable'
 
-const theme = {
-  global: {
-    colors: {
-      'main': '#ffd602',
-      'sub': '#B721FF'
-    },
-    font: {
-      // size: '40px',
-      // family: ''
-    },
-    hover: {
-      dark: '#B721FF'
-    }
-  }
-}
 function App() {
   return (
-    <Grommet theme={theme}>
-      <DataTable />
-    </Grommet>
+    <DataTable />
   );
 }
 
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,58 +1,108 @@
 import React, { useState } from 'react'
-import { Button, Grommet, Box, Collapsible } from 'grommet'
-import { Notification } from 'grommet-icons'
-
-const customTheme = {
-  button: {
-    width: "20px",
-    border: {
-      radius: '50px',
-      color: '#B721FF'
-    },
-    primary: {
-      color: "#ffd602"
-    }
-  }
-};
+import { Edit, Trash } from 'grommet-icons'
 
 const defaultData = [
   {
-    "id": "1",
-    "name": "name1",
-    "occupation": "student"
+    "sku": "1",
+    "productName": "name1",
+    "price": "13",
+    "seller": "Kate"
   }, {
-    "id": "2",
-    "name": "name2",
-    "occupation": "engineer"
+    "sku": "2",
+    "productName": "name2",
+    "price": "12",
+    "seller": "Kate"
   }, {
-    "id": "3",
-    "name": "name3",
-    "occupation": "doctor"
-  },
+    "sku": "3",
+    "productName": "name3",
+    "price": "10",
+    "seller": "Kate"
+  }
 ]
 
 const DataTable = () => {
-  const [isAuthorized, setAuthorize] = useState(false)
-  const [data, setData] = useState(defaultData)
+  const [data, setUpdateData] = useState(defaultData)
+  const [inputs, setInputs] = useState({})
+  const [sku, setSku] = useState('')
+  const [productName, setProductName] = useState('')
+  const [seller, setSeller] = useState('')
+  const [price, setPrice] = useState(0)
+
+  const handleCreateData = (e) => {
+    const inputEls = document.querySelectorAll('input')
+    if (inputs.sku && inputs.productName && inputs.seller && inputs.price) {
+      e.preventDefault();
+      setUpdateData([...data, inputs])
+      inputEls.forEach(i => (i.value = ''))
+      setInputs({})
+    } else alert("Fill in the blank field(s)")
+  }
+
+  const handleInputChange = (e) => {
+    e.persist();
+    setInputs(inputs => ({ ...inputs, [e.target.name]: e.target.value }));
+  }
 
   return (
-    <Grommet theme={customTheme}>
-      <Button style={{ width: '200px' }} primary icon={<Notification />} label="Load Data" hoverIndicator onClick={() => { setAuthorize(!isAuthorized) }} />
-      <Collapsible direction="vertical" open={isAuthorized}>
-        <Box flex
-          width='medium'
-          background='light-2'
-          elevation='small'
-          align='center'
-          justify='center'>
-          <ul>
-            {data.map(i => (
-              <li>{i.name}</li>
-            ))}
-          </ul>
-        </Box>
-      </Collapsible>
-    </Grommet>
+    <React.Fragment>
+      <div className="container text-center">
+        <h1 className="bg-light py-4 text-info">
+          Electronic Store
+        </h1>
+        <div className="d-flex justify-content-center">
+          <form className="w-50">
+            <input type="text" id="sku" className="form-control" placeholder="SKU#" onChange={handleInputChange} name="sku" value={inputs.sku} />
+            <input type="text" id="proname" className="form-control" placeholder="Product Name" name="productName" onChange={handleInputChange} value={inputs.productName} />
+            <div className="row">
+              <div className="col">
+                <input type="text" id="seller" className="form-control m-0" placeholder="Seller" name="seller" onChange={handleInputChange} value={inputs.seller} />
+              </div>
+              <div className="col">
+                <input type="text" id="price" className="form-control m-0" placeholder="Price" name="price" onChange={handleInputChange} value={inputs.price} />
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-success" type="button" id="btn-create" onClick={handleCreateData}>Create</button>
+          <button className="btn btn-primary" type="button" id="btn-read">Read</button>
+          <button className="btn btn-warning" type="button" id="btn-update">Update</button>
+          <button className="btn btn-danger" type="button" id="btn-delete">Delete All</button>
+        </div>
+
+        <div className="d-flex table-data">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">SKU</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Seller</th>
+                <th scope="col">Price</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody id="tbody">
+              {data.map(i => (
+                <tr key={i.sku}>
+                  <td>{i.sku}</td>
+                  <td>{i.productName}</td>
+                  <td>{i.seller}</td>
+                  <td>{i.price}</td>
+                  <td><Edit /></td>
+                  <td><Trash /></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div id="notfound"></div>
+      </div>
+
+      <div className="w-100 btn btn-success insertmsg">Data Inserted Successfully...!</div>
+      <div className="w-100 btn btn-warning updatemsg">Data Updated..! Refresh Database To See Result</div>
+      <div className="w-100 btn btn-danger deletemsg">Data Deleted..!</div>
+    </React.Fragment>
   )
 }
 
