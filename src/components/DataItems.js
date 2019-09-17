241cee98232a3a7d8ef8667e36bd2ddf09b28f40
Beigenut <beigenut@gmail.com>
2019-09-17 02:09:33 -0400

    Table Design

--- src/components/DataItems.js
+++ src/components/DataItems.js
@@ -1,14 +1,47 @@
 import React from 'react'
+import { Form, Col } from 'react-bootstrap'
 import { Edit, Trash } from 'grommet-icons'
 
 const DataItems = (props) => {
   const { sku, productName, price, seller, onDelete } = props
   return (
     < tr >
-      <td>{sku}</td>
-      <td>{productName}</td>
-      <td>{seller}</td>
-      <td>{price}</td>
+      <td>
+        <Form>
+          <Form.Group controlId="skuForm">
+            <Col sm="10">
+              <Form.Control plaintext readOnly defaultValue={sku} />
+            </Col>
+          </Form.Group>
+        </Form>
+      </td>
+      <td>
+        <Form>
+          <Form.Group controlId="productNameForm">
+            <Col sm="10">
+              <Form.Control plaintext readOnly defaultValue={productName} />
+            </Col>
+          </Form.Group>
+        </Form>
+      </td>
+      <td>
+        <Form>
+          <Form.Group controlId="sellerForm">
+            <Col sm="10">
+              <Form.Control plaintext readOnly defaultValue={seller} />
+            </Col>
+          </Form.Group>
+        </Form>
+      </td>
+      <td>
+        <Form>
+          <Form.Group controlId="priceForm">
+            <Col sm="10">
+              <Form.Control plaintext readOnly defaultValue={price} />
+            </Col>
+          </Form.Group>
+        </Form>
+      </td>
       <td><Edit /></td>
       <td><Trash onClick={e => onDelete(sku)} /></td>
     </tr >
